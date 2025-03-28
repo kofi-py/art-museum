ed6187dab566fb0bf282169051cfe11d0318369f
Kofi Adu-Boateng <pastorkofi101@gmail.com>
2025-03-27 21:56:05 -0400

    Remember your <NavLink>s in <GalleryNavigation>? This <Route> is where they will lead! You will need to use both the <Route> and <GalleryView> components in App.js. Now that another element is being added to App.js, you will need to wrap <GalleryNavigation> and <Route> in something. We use <div> with a className "page-wrapper". The path prop of the <Route> component works a lot like the definition of an Express route! Submit a Progress Tracker question if you get stuck!

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import GalleryNavigation from "./components/GalleryNavigation";
 import GalleryView from "./components/GalleryView";
-import harvardArt from "./data/harvardArt"; // Ensure correct import
+import harvardArt from "./data/harvardArt"; // Ensure this is correctly imported
 
 function App() {
   return (
-    <BrowserRouter>
-      <GalleryNavigation galleries={harvardArt.records} />
-      <Route path="/" element={<h2>Welcome to the Harvard Art Museum</h2>} />
-      <Route path="/galleries/:galleryId" element={<GalleryView />} />
-    </BrowserRouter>
+    <Router>
+      <div className="page-wrapper">
+        <GalleryNavigation galleries={harvardArt.records} />
+        <Switch>
+          <Route
+            path="/galleries/:galleryId"
+            element={<GalleryView galleries={harvardArt.records} />}
+          />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
