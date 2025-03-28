b3d05d22cb7196c29de66532da5b69192d1fb9d3
Kofi Adu-Boateng <pastorkofi101@gmail.com>
2025-03-27 21:59:00 -0400

    In the GalleryView component, extract the matched value for the galleryId URL parameter with the useParams hook from React Router. Print this value and make sure it looks correct in the browser's Dev Tools console. See how far you can get with just the documentation. Use Progress Tracker if you are stuck! Again: the param you are looking for is galleryId! (You set the parameter's name in the Route path that you specified in App.js.)

--- src/components/GalleryView/index.js
+++ src/components/GalleryView/index.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
-const GalleryView = () => {
+const GalleryView = ({ galleries }) => {
+  const { galleryId } = useParams();
+  console.log("Gallery ID from URL:", galleryId);
+
   return <h1>Hello from GalleryView</h1>;
 };
 
