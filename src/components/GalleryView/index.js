75891403c28d67416029a017d6b62d1d1e9090d2
Kofi Adu-Boateng <pastorkofi101@gmail.com>
2025-03-27 21:59:38 -0400

    Extract the correct gallery based on galleryId.

--- src/components/GalleryView/index.js
+++ src/components/GalleryView/index.js
@@ -3,9 +3,15 @@ import { useParams } from "react-router-dom";
 
 const GalleryView = ({ galleries }) => {
   const { galleryId } = useParams();
-  console.log("Gallery ID from URL:", galleryId);
+  const gallery = galleries.find((g) => g.id.toString() === galleryId);
 
-  return <h1>Hello from GalleryView</h1>;
+  console.log("Gallery Data:", gallery);
+
+  return (
+    <div>
+      <h1>{gallery ? gallery.name : "Gallery Not Found"}</h1>
+    </div>
+  );
 };
 
 export default GalleryView;
